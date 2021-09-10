6f2ef2ab64e0ceda39f5582fce757fdcbce5abe7
TazMania <nallinon@gmail.com>
2021-09-10 01:04:36 -0300

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  bool _enableBtn = false;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController subjectController = TextEditingController();
+  TextEditingController messageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,17 +19,25 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Email Sender'),
         ),
-        body: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          children: [
-            _createSubjectInput(),
-            Divider(),
-            _createEmailInput(),
-            Divider(),
-            _createTextInput(),
-            Divider(),
-            _createSubmitButton(),
-          ],
+        body: Form(
+          key: _formKey,
+          onChanged: () {
+            setState() {
+              _enableBtn = _formKey.currentState!.validate();
+            }
+          },
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            children: [
+              _createSubjectInput(),
+              Divider(),
+              _createEmailInput(),
+              Divider(),
+              _createTextInput(),
+              Divider(),
+              _createSubmitButton(),
+            ],
+          ),
         ),
       ),
     );
@@ -30,7 +45,7 @@ class MyApp extends StatelessWidget {
 
   Widget _createSubjectInput() {
     return TextField(
-      // autofocus: true,
+      controller: subjectController,
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
@@ -42,7 +57,16 @@ class MyApp extends StatelessWidget {
   }
 
   Widget _createEmailInput() {
-    return TextField(
+    return TextFormField(
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Email is required';
+        } else if (!value.contains('@')) {
+          return 'Please enter a valid email address';
+        }
+        return null;
+      },
+      controller: emailController,
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
@@ -55,6 +79,7 @@ class MyApp extends StatelessWidget {
 
   Widget _createTextInput() {
     return TextField(
+      controller: messageController,
       maxLines: 10,
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
@@ -68,8 +93,22 @@ class MyApp extends StatelessWidget {
 
   Widget _createSubmitButton() {
     return ElevatedButton(
-      onPressed: () {},
-      child: Text('Submit', style: TextStyle(fontSize: 20),),
+      onPressed: _enableBtn
+          ? (() async {
+              final Email email = Email(
+                body: messageController.text,
+                subject: subjectController.text,
+                recipients: [emailController.text],
+                isHTML: false,
+              );
+
+              await FlutterEmailSender.send(email);
+            })
+          : null,
+      child: Text(
+        'Submit',
+        style: TextStyle(fontSize: 20),
+      ),
       style: ElevatedButton.styleFrom(
         fixedSize: Size(50, 50),
         primary: Colors.blue,
