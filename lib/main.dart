623efce793512230fe10236f6699dbc49ea55000
TazMania <nallinon@gmail.com>
2021-09-10 16:07:47 -0300

    Working

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,6 @@ import 'package:flutter_email_sender/flutter_email_sender.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final _formKey = GlobalKey<FormState>();
-  bool _enableBtn = false;
-  TextEditingController emailController = TextEditingController();
-  TextEditingController subjectController = TextEditingController();
-  TextEditingController messageController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,32 +13,69 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Email Sender'),
         ),
-        body: Form(
-          key: _formKey,
-          onChanged: () {
-            setState() {
-              _enableBtn = _formKey.currentState!.validate();
-            }
-          },
-          child: ListView(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            children: [
-              _createSubjectInput(),
-              Divider(),
-              _createEmailInput(),
-              Divider(),
-              _createTextInput(),
-              Divider(),
-              _createSubmitButton(),
-            ],
-          ),
-        ),
+        body: MyForm(),
+      ),
+    );
+  }
+}
+
+class MyForm extends StatefulWidget {
+  MyForm({Key? key}) : super(key: key);
+
+  @override
+  _MyFormState createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  final _formKey = GlobalKey<FormState>();
+  bool _enableBtn = false;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController subjectController = TextEditingController();
+  TextEditingController messageController = TextEditingController();
+  String? _validatorInputs(value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter some text';
+    }
+    return null;
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailController.dispose();
+    subjectController.dispose();
+    messageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      onChanged: (() {
+        setState(() {
+          _enableBtn = _formKey.currentState!.validate();
+        });
+      }),
+      key: _formKey,
+      child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        children: [
+          _createSubjectInput(),
+          Divider(),
+          _createEmailInput(),
+          Divider(),
+          _createTextInput(),
+          Divider(),
+          _createSubmitButton(),
+        ],
       ),
     );
   }
 
   Widget _createSubjectInput() {
-    return TextField(
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: _validatorInputs,
       controller: subjectController,
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
@@ -58,14 +89,8 @@ class MyApp extends StatelessWidget {
 
   Widget _createEmailInput() {
     return TextFormField(
-      validator: (value) {
-        if (value!.isEmpty) {
-          return 'Email is required';
-        } else if (!value.contains('@')) {
-          return 'Please enter a valid email address';
-        }
-        return null;
-      },
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: _validatorInputs,
       controller: emailController,
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
@@ -78,7 +103,9 @@ class MyApp extends StatelessWidget {
   }
 
   Widget _createTextInput() {
-    return TextField(
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: _validatorInputs,
       controller: messageController,
       maxLines: 10,
       textCapitalization: TextCapitalization.sentences,
@@ -93,18 +120,7 @@ class MyApp extends StatelessWidget {
 
   Widget _createSubmitButton() {
     return ElevatedButton(
-      onPressed: _enableBtn
-          ? (() async {
-              final Email email = Email(
-                body: messageController.text,
-                subject: subjectController.text,
-                recipients: [emailController.text],
-                isHTML: false,
-              );
-
-              await FlutterEmailSender.send(email);
-            })
-          : null,
+      onPressed: _validate,
       child: Text(
         'Submit',
         style: TextStyle(fontSize: 20),
@@ -115,5 +131,22 @@ class MyApp extends StatelessWidget {
         shape: StadiumBorder(),
       ),
     );
+  }
+
+  void _validate() {
+    print(_enableBtn);
+    // _formKey.currentState!.validate()
+    _enableBtn ? _sendEmail() : print("no funciona pa");
+  }
+
+  void _sendEmail() async {
+    print("Funciona pa");
+    final Email email = Email(
+      body: messageController.text,
+      subject: subjectController.text,
+      recipients: [emailController.text],
+      isHTML: false,
+    );
+    await FlutterEmailSender.send(email);
   }
 }
